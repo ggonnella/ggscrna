d35ea5fdd73b05deeee52d9963ba01ad909db719
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2023-09-21 08:09:56 +0700

    get_step_dir

--- R/ggscrna.R
+++ R/ggscrna.R
@@ -322,3 +322,23 @@ source_rmd <- function(file, ...) {
   source(file <- tmp_file, ...)
 }
 
+#' Get the path to a step directory
+#'
+#' @param steps_dir String. Path to the "steps" directory.
+#' @param step_n String. Prefix of the step.
+#'
+#' @return String. Path to the step directory.
+get_step_dir <- function(steps_dir, step_n) {
+  all_files <- list.files(path = steps_dir)
+  matching_files <- all_files[grepl(paste0("^", step_n), all_files)]
+  if (length(matching_files) == 0) {
+    stop(paste0("No entries matching '",
+                step_n, "' in '", steps_dir, "'"))
+  }
+  if (length(matching_files) > 1) {
+    stop(paste0("Multiple entries matching '",
+                step_n, "' in '", steps_dir, "'"))
+  }
+  paste0(steps_dir, "/", matching_files[1], "/")
+}
+
