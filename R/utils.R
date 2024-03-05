5ed9ab939d43f0ed823204ddf296a624de577bbd
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2024-03-05 10:49:44 +0700

    cleanup code in utils.R

--- R/utils.R
+++ R/utils.R
@@ -3,9 +3,10 @@
 #'
 
 # Licence: CC-BY-SA
-# (c) Giorgio Gonnella, 2023
+# (c) Giorgio Gonnella, 2023-2024
 
-#' Include a Rmd file in another
+#' Call the code of a Rmd file in another Rmd or a R script.
+#' The code of the Rmd file is extracted as R script and then sourced. 
 #'
 #' @param file Filename of the file to be included
 #'
@@ -18,21 +19,21 @@ source_rmd <- function(file, ...) {
 
 #' Get the path to a step directory
 #'
-#' @param steps_dir String. Path to the "steps" directory.
-#' @param step_n String. Prefix of the step.
+#' @param steps_dir  String.  Path to the "steps" directory.
+#' @param step_n     String.  Prefix of the step.
 #'
-#' @return String. Path to the step directory.
+#' @return           String.  Path to the step directory.
+#' 
 get_step_dir <- function(steps_dir, step_n) {
-  all_files <- list.files(path = steps_dir)
+  all_files      <- list.files(path = steps_dir)
   matching_files <- all_files[grepl(paste0("^", step_n), all_files)]
-  if (length(matching_files) == 0) {
-    stop(paste0("No entries matching '",
-                step_n, "' in '", steps_dir, "'"))
-  }
-  if (length(matching_files) > 1) {
-    stop(paste0("Multiple entries matching '",
-                step_n, "' in '", steps_dir, "'"))
-  }
+
+  # there should be only one match
+  if (length(matching_files) == 0)
+    stop(paste0("No entry matches '",       step_n, "' in '", steps_dir, "'"))
+  else if (length(matching_files) > 1)
+    stop(paste0("Multiple entries match '", step_n, "' in '", steps_dir, "'"))
+
   paste0(steps_dir, "/", matching_files[1], "/")
 }
 
